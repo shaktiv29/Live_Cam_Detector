95b8ae8dd14f874b696bf80acc4ed8dd79317e74
Shakti Vishwakarma <48163793+shaktiji@users.noreply.github.com>
2020-03-31 13:55:27 +0530

    Add files via upload

---
+++ formodel.py
@@ -0,0 +1,74 @@
+import numpy as np
+import tensorflow as tf
+import pathlib
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+import cv2
+from object_detection.utils import visualization_utils as vis_util
+
+utils_ops.tf = tf.compat.v1
+tf.gfile = tf.io.gfile
+
+def load_model(model_name):
+  PATH_TO_LABELS = 'faster_rcnn_inception_v2_coco_2018_01_28/new_mscoco.pbtxt'
+  category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+  base_url = 'http://download.tensorflow.org/models/object_detection/'
+  model_file = model_name + '.tar.gz'
+  model_dir = tf.keras.utils.get_file(
+    fname=model_name,
+    origin=base_url + model_file,
+    untar=True)
+
+  model_dir = pathlib.Path(model_dir)/"saved_model"
+
+  model = tf.saved_model.load(str(model_dir))
+  model = model.signatures['serving_default']
+
+  return model, category_index
+
+def run_inference_for_single_image(model, image, thresh):
+  input_tensor = tf.convert_to_tensor(image)
+  input_tensor = input_tensor[tf.newaxis,...]
+  output_dict = model(input_tensor)
+  num_detections = int(output_dict.pop('num_detections'))
+  output_dict = {key:value[0, :num_detections].numpy()
+                 for key,value in output_dict.items()}
+  output_dict['num_detections'] = num_detections
+  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+  if 'detection_masks' in output_dict:
+    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+              output_dict['detection_masks'], output_dict['detection_boxes'],
+               image.shape[0], image.shape[1])
+    detection_masks_reframed = tf.cast(detection_masks_reframed > thresh,
+                                       tf.uint8)
+    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+  return output_dict
+
+def show_inference(model, image,th, category_index):
+  image_np = np.array(image)
+  output_dict = run_inference_for_single_image(model, image_np, thresh=th)
+  vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      instance_masks=output_dict.get('detection_masks_reframed', None),
+      use_normalized_coordinates=True,
+      line_thickness=3)
+  return image_np
+
+def yo():
+    model_name = 'ssd_mobilenet_v1_coco_2018_01_28'
+    detection_model, category_index = load_model(model_name)
+    thresh = 0.5
+    cap = cv2.VideoCapture(0)
+    while(cap.isOpened()):
+        _,frame = cap.read()
+        p = show_inference(detection_model,frame, thresh, category_index)
+        cv2.imshow("Images Detector made by Shakti" , p)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+yo()
